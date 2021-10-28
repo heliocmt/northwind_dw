15bb1a68bc88b376b70bfc107091a002d3a29896
hcmtavora@gmail.com <hcmtavora@gmail.com>
2021-10-28 22:29:36 +0000

    Compilou tudo

--- models/northwind/mart/fact_orders.sql
+++ models/northwind/mart/fact_orders.sql
@@ -9,7 +9,18 @@ with
     ),
 
     products as (
-        select *
+        select
+        product_sk 
+        , product_id 
+        , product_name 
+        , supplier_id 
+        , category_id 
+        , quantity_per_unit 
+        , unit_price 
+        , units_in_stock 
+        , units_on_order 
+        , reorder_level 
+        , discontinued
         from {{ ref('dim_products') }}
     ),
 
@@ -29,70 +40,90 @@ with
         , company_name
         from {{ ref('dim_suppliers') }}
     ),
-    
+
     order_date as (
         select
         order_date_sk
+        , order_date
         , order_id
         from {{ ref('dim_date') }}
     ),
 
     order_with_sk as (
         select
-        orders.order_id
-        , customers.customer_sk as customer_fk
+        customers.customer_sk as customer_fk
         , order_date.order_date_sk as order_date_fk
         , employees.employee_sk as employee_fk
-        , orders.order_date  
-        , orders.shipped_date 
-        , orders.required_date
-        , orders.freight
-        , orders.ship_name
-        , orders.ship_address
-        , orders.ship_city
-        , orders.ship_region
-        , orders.ship_postal_code
-        , orders.ship_country
-        from {{ ref('stg_orders') }} as orders
-        left join customers on orders.customer_id = customers.customer_id
-        left join employees on orders.employee_id = employees.employee_id
-        left join order_date on orders.order_id = order_date.order_id
+        , order_date.order_id as order_id
+        , order_date  
+        , shipped_date 
+        , required_date
+        , freight
+        , ship_name
+        , ship_address
+        , ship_city
+        , ship_region
+        , ship_postal_code
+        , ship_country
+        from {{ ref('stg_orders') }} as order_with_sk
+        left join customers on order_with_sk.customer_id = customers.customer_id
+        left join employees on order_with_sk.employee_id = employees.employee_id
+        left join order_date on order_with_sk.order_date = order_date.order_date
     ),
     
+    order_details as (
+        select 
+        order_id
+        , products.product_sk as product_sk
+        , products.product_name as product_name
+        , products.unit_price as unit_price
+        , quantity  
+        , discount  
+        , products.supplier_id as supplier_id
+        , products.category_id as category_id
+        , products.quantity_per_unit as quantity_per_unit
+        , products.units_in_stock as unit_in_stock
+        , products.units_on_order as unit_on_order
+        from {{ ref('stg_order_details') }} as order_details
+        left join products on order_details.product_id = products.product_id
+    ),
+
     order_details_with_sk as (
-        select
-        products.product_sk as product_fk
-        , suppliers.supplier_sk as supplier_fk
-        , products.product_id as product_id
-        , suppliers.supplier_id
-        , order_details.order_id as order_id
-        , order_details.unit_price as unit_price
-        , order_details.quantity as quantity
-        , order_details.discount as discount
-        from {{ ref('stg_order_details') }} as order_details_with_sk
-        left join products on order_details_with_sk.product_id = products.product_id
-        left join suppliers on order_details_with_sk.supplier_id = suppliers.supplier_id 
+        select 
+        order_details.order_id
+        , order_details.product_sk as product_fk
+        , order_details.product_name
+        , order_details.unit_price 
+        , order_details.quantity  
+        , order_details.discount 
+        , order_details.category_id
+        , order_details.quantity_per_unit
+        , order_details.unit_in_stock
+        , order_details.unit_on_order
+        , supplier_sk as supplier_fk
+        , company_name
+        from suppliers as order_details_with_sk
+        left join order_details on order_details_with_sk.supplier_id = order_details.supplier_id 
     ),
     
     final as (
         select
-        order_details_with_sk.order_id
-        , order_with_sk.customer_fk 
+        order_details_with_sk.order_id as order_id
+        , customer_fk 
         , order_details_with_sk.product_fk
-        , order_with_sk.order_fk
-        , order_with_sk.order_date_fk
-        , order_with_sk.employee_fk
-        , order_with_sk.supplier_fk
-        , order_with_sk.order_date  
-        , order_with_sk.shipped_date 
-        , order_with_sk.required_date  
-        , order_with_sk.freight 
-        , order_with_sk.ship_name  
-        , order_with_sk.ship_address 
-        , order_with_sk.ship_city 
-        , order_with_sk.ship_region  
-        , order_with_sk.ship_postal_code 
-        , order_with_sk.ship_country
+        , order_date_fk
+        , employee_fk
+        , supplier_fk
+        , order_date  
+        , shipped_date 
+        , required_date  
+        , freight 
+        , ship_name  
+        , ship_address 
+        , ship_city 
+        , ship_region  
+        , ship_postal_code 
+        , ship_country
         , order_details_with_sk.unit_price 
         , order_details_with_sk.quantity 
         , order_details_with_sk.discount  
